94ac5e53ac5279ae142b4288d4bdfd8faf7bf8b5
Nathan <nathanchew.24@ichat.sp.edu.sg>
2025-04-28 16:33:31 +0800

    exercise 2 n 3

--- practice 2.py
+++ practice 2.py
@@ -1,9 +1,23 @@
 import math
-
+import statistics as st
 def main():
+    temp=[]
+    templist=[]
+    min_max=[]
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     bmi=calculate_bmi(weight=57, height=1.73)
     bmi_range(bmi)
+    display_main_menu()
+    templist=get_user_input()
+    print(templist)
+    calc_average(templist)
+    min_max=find_min_max(templist)
+    print("Minimum temperature:",min_max[0],"\nMaximum temperature:",min_max[1])
+    temp=sort_temperature(templist)
+    print(f"Sorted temperature list: {temp}")
+    median=calc_median_temperature(temp)
+    print("The median temperature is",median)
+
 
 
 def calculate_bmi(height, weight):
@@ -16,10 +30,51 @@ def calculate_bmi(height, weight):
 def bmi_range(bmi):
     if bmi>25:
         print("Over Weight")
-    elif 18.5<bmi<=25:
+    elif 18.5<=bmi<=25:
         print("Normal Weight")
     else:
         print("Under Weight")
+
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    value=0
+    temp=[]
+    while True:
+        value=float(input("Press -1 to terminate\nEnter temp:")) 
+        if value==-1:
+            break 
+        temp.append(value)
+    return temp
+
+def calc_average(templist):
+    sum=0
+    for num in templist:
+        sum+=num
+    avg=sum/(len(templist))
+    return avg
     
+def find_min_max(templist):
+    max=templist[0]
+    min=templist[0]
+    for num in templist:
+        if num>max:
+            max=num
+        elif num<min:
+            min=num
+    return [min,max]
+
+def sort_temperature(templist):
+    sorted_templist = sorted(templist)
+    return sorted_templist
+
+def calc_median_temperature(sorted_templist):
+    median=st.median(sorted_templist)
+    return median
+
+
+
+
 if __name__=='__main__':
     main()
